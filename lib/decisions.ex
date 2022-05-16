4ac1c630717156e3901508added23ac21edeafd2
Jules <jroadknight@8thlight.com>
2022-05-16 13:33:29 +0100

    Matches decision to result of choice

--- lib/decisions.ex
+++ lib/decisions.ex
@@ -1,25 +1,29 @@
 defmodule Decisions do
   def make_decision(_person, decisions_path) do
-    { _status, decisions_list} = get_decisions(decisions_path)
+    { _status, decisions_list } = get_decisions(decisions_path)
     evaluate_choices(decisions_list)
   end
 
-  defp get_decisions(decisions_path) do
+  def get_decisions(decisions_path) do
     with {:ok, contents} <- File.read("./lib/decisions/" <> decisions_path),
       {:ok, json} <- Jason.decode(contents), do: {:ok, json}
   end
 
   defp evaluate_choices(decisions) do
     if length(decisions) > 0 do
-      sum_choices(decisions, 0)
+      sum_choices(decisions) |> :rand.uniform |> decision_result(decisions)
     else
       "Do nothing"
     end
   end
 
-  defp sum_choices(decisions, total_chance) do
-    for choice <- decisions do
-      sum_choices(decisions, total_chance + choice[:chance])
-    end
+  def sum_choices(decisions) do
+    Enum.reduce(decisions, 0, fn choice, acc -> choice["chance"] + acc end)
+  end
+
+  def decision_result(result, decisions) do
+    Enum.reduce_while(decisions, 0, fn choice, acc ->
+      if choice["chance"] + acc < result, do: {:cont, acc + choice["chance"]}, else: {:halt, choice["name"]}
+    end)
   end
 end
--- test/decisions_test.exs
+++ test/decisions_test.exs
@@ -25,4 +25,25 @@ defmodule DecisionsTest do
     decisions = "twoDecisions.json"
     assert Decisions.make_decision(person, decisions) in ["Win", "Lose"]
   end
+
+  test "sums total choice of decisions" do
+    person = %Person{id: 1, genes: "aaaa"}
+    decisions = "twoDecisions.json"
+    { _status, decisions_list} = Decisions.get_decisions(decisions)
+    assert Decisions.sum_choices(decisions_list) == 1010
+  end
+
+  test "finds matching result of decision" do
+    person = %Person{id: 1, genes: "aaaa"}
+    decisions = "twoDecisions.json"
+    { _status, decisions_list} = Decisions.get_decisions(decisions)
+    assert Decisions.decision_result(10, decisions_list) == "Win"
+  end
+
+  test "finds other matching result of decision" do
+    person = %Person{id: 1, genes: "aaaa"}
+    decisions = "twoDecisions.json"
+    { _status, decisions_list} = Decisions.get_decisions(decisions)
+    assert Decisions.decision_result(1010, decisions_list) == "Lose"
+  end
 end
