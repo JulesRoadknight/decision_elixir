1ed6843fcdb251acde7e8e2cf4961b7f77232084
Jules <jroadknight@8thlight.com>
2022-07-11 16:56:43 +0100

    Calculates equity with modifiers for a player

--- lib/equity.ex
+++ lib/equity.ex
@@ -1,8 +1,15 @@
 defmodule Equity do
 
-  def decisions_equity(decisions_path) do
+  def unmodified_decisions_equity(decisions_path) do
     { _status, decisions_list } = Decisions.get_decisions(decisions_path)
-    Enum.reduce(decisions_list, 0, fn decision, acc -> decision["chance"] * Results.match_decision_result(decision["name"], decisions_path) + acc end) /
-    Decisions.sum_chance_of_choices(decisions_list)
+    Enum.reduce(decisions_list, 0, fn decision, acc -> decision["chance"] * Results.match_decision_result(decision["name"], decisions_path) + acc end)
+    / Decisions.sum_chance_of_choices(decisions_list)
+  end
+
+  def person_total_equity(person, decisions_path) do
+    { _status, decisions_list } = Decisions.get_decisions(decisions_path)
+    updated_decisions = Decisions.apply_modifiers(person, decisions_list)
+    Enum.reduce(updated_decisions, 0, fn decision, acc -> decision["chance"] * Results.match_decision_result(decision["name"], decisions_path) + acc end)
+    / Decisions.sum_chance_of_choices(updated_decisions)
   end
 end
--- test/equity_test.exs
+++ test/equity_test.exs
@@ -3,8 +3,24 @@ defmodule EquityTest do
   doctest Equity
 
   test "#decisions_equity returns unmodified totals of equity" do
-    person = %Person{id: 1, genes: "aaaa", survival_chance: 100}
     decisions_path = "test/naturalSelectionFiveDecisions.json"
-    assert Equity.decisions_equity(decisions_path) == -0.3
+    assert Equity.unmodified_decisions_equity(decisions_path) == -0.3
+  end
+
+  test "#decisions_equity returns 0.0 for neutral equity" do
+    decisions_path = "test/twoDecisions.json"
+    assert Equity.unmodified_decisions_equity(decisions_path) == 0.0
+  end
+
+  test "#person_total_equity returns equity after modifiers (without certainties)" do
+    person = %Person{id: 1, genes: "Cmaf", survival_chance: 100}
+    decisions_path = "test/naturalSelectionFiveDecisions.json"
+    assert Equity.person_total_equity(person, decisions_path) |> Float.round(2) == -2.86
+  end
+
+  test "#person_total_equity returns better equity with more favourable genes" do
+    person = %Person{id: 1, genes: "jQTZ", survival_chance: 100}
+    decisions_path = "test/naturalSelectionFiveDecisions.json"
+    assert Equity.person_total_equity(person, decisions_path) |> Float.round(2) == 0.83
   end
 end
